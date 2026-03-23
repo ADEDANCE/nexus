b47b0009739c79a42f68eefadc9e7858eac2e9ce
Adedance <uthmanadedance@gmail.com>
2026-03-23 18:07:40 +0100

    Refractor(Add rounded conner)

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ children, className, onClick }) => {
   return (
-    <button onClick={onClick} className={` py-3 px-6 ${className}`}>
+    <button onClick={onClick} className={` py-3 px-6 rounded-2xl ${className}`}>
       {children}
     </button>
   );
