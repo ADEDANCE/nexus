ca49a1f41699f4b47294fe38a6a0c2990987d7de
Adedance <uthmanadedance@gmail.com>
2026-03-23 23:26:32 +0100

    Refractor(Make responsive across the screens)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
   return (
     <footer className=" bg-white border-t border-gray-300 shadow py-4 px-4">
       <div className=" flex  flex-col md:flex-row  text-gray-600  justify-between items-center">
-        <div className=" flex items-center gap-2">
-          <FaRegCopyright />
-          <p>2026 Nexus. Helping farmers grow healthier crops.</p>
-        </div>
+        <p className=" text-center ">
+          © 2026 Nexus. Helping farmers grow healthier crops.
+        </p>
+
         <div className=" flex gap-4">
           <Link className=" hover:text-green-600">About</Link>
           <Link className=" hover:text-green-600">Contact</Link>
