c26b99cd30414888c05cd9a9a894fbf58bc6cbae
Adedance <uthmanadedance@gmail.com>
2026-03-23 23:28:27 +0100

    Import the new section created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "../section/Hero";
 import About from "../section/About";
+import Mission from "../section/Mission";
+import Getstarted from "../section/Getstarted";
 
 const Home = () => {
   return (
     <div className=" bg-green-50 ">
       <Hero />
       <About />
+      <Mission />
+      <Getstarted />
     </div>
   );
 };
