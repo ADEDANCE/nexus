dbade3edd68ccc7aefb11b3ad679a001cb697b44
Adedance <uthmanadedance@gmail.com>
2026-03-23 18:09:14 +0100

    Install react icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import Navber from "./components/Navber";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
+      <Navber />
       <main className="grow">
-        <Navber />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </main>
+      <Footer />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Hero from "../section/Hero";
+import About from "../section/About";
 
 const Home = () => {
-  return <div className=" bg-green-50">Home</div>;
+  return (
+    <div className=" bg-green-50 ">
+      <Hero />
+      <About />
+    </div>
+  );
 };
 
 export default Home;
