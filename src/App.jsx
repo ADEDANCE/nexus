7381304573f25f71b4821fc0e07c18892d3554e9
Adedance <uthmanadedance@gmail.com>
2026-03-24 16:42:44 +0100

    Import the newly created page route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import Navber from "./components/Navber";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Dashboard from "./pages/Dashboard";
+import History from "./pages/History";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
@@ -10,6 +15,11 @@ function App() {
       <main className="grow">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgetpassword" element={<ForgotPassword />} />
         </Routes>
       </main>
       <Footer />
