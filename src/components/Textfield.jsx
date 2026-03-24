381362369042ec10e5236904f61d3dd9963c0879
Adedance <uthmanadedance@gmail.com>
2026-03-24 22:14:46 +0100

    Refractor(Add password type and make it op tional)

--- src/components/Textfield.jsx
+++ src/components/Textfield.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import { useState } from "react";
 
 const Textfield = ({
   label,
@@ -8,20 +10,29 @@ const Textfield = ({
   onChange,
   className = "",
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <div className="flex flex-col gap-1 w-full">
+    <div className="flex flex-col gap-1 w-full relative">
       {/* Label */}
       {label && (
         <label className="text-sm font-medium text-black">{label}</label>
       )}
 
       <input
-        type={type}
+        type={type === "password" && showPassword ? "text" : type}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        className={`border border-gray-300 rounded-lg px-3 py-2 outline-none focus:ring-2 focus:ring-green-500 ${className}`}
+        className={`border border-gray-300 rounded-lg px-3 py-2 outline-none focus:ring-2 focus:ring-green-500 pr-10 ${className}`}
       />
+      {type === "password" && (
+        <span
+          onClick={() => setShowPassword(!showPassword)}
+          className="absolute right-3 bottom-1 transform -translate-y-1/2 cursor-pointer text-gray-500"
+        >
+          {showPassword ? <FiEyeOff /> : <FiEye />}
+        </span>
+      )}
     </div>
   );
 };
